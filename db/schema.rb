e2bf575fe4edbe5061e5fee623a5553b097013f6
phamchien98 <phamchien22598hn@gmail.com>
2021-03-11 17:40:05 +0700

    add devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_11_085248) do
+ActiveRecord::Schema.define(version: 2021_03_11_102219) do
 
   create_table "activities", charset: "utf8mb4", force: :cascade do |t|
     t.integer "status"
@@ -39,7 +39,12 @@ ActiveRecord::Schema.define(version: 2021_03_11_085248) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "activities", "users"
